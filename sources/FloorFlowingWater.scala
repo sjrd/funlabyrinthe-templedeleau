65fb66f4c43d11aaa0532b804cb89bc636f301e5
Sébastien Doeraene <sjrdoeraene@gmail.com>
2026-08-13 23:38:26 +0200

    Floor flowing water and blocks.

---
+++ sources/FloorFlowingWater.scala
@@ -0,0 +1,137 @@
+package user.sjrd.templedeleau
+
+import com.funlabyrinthe.core.*
+import com.funlabyrinthe.core.scene.*
+import com.funlabyrinthe.mazes.*
+import com.funlabyrinthe.mazes.std.*
+
+object FloorFlowingWater extends Module
+
+def CatFloorFlowingWater(using Universe) =
+  ComponentCategory("floorFlowingWater", "Floor flowing water")
+
+@definition def flowingWaterInfos(using Universe) = FlowingWaterInfos()
+
+@definition def platformGroundTemplate(using Universe) = PlatformGround().asTemplate()
+@definition def platformWallTemplate(using Universe) = PlatformWall().asTemplate()
+@definition def changeWaterFloorButtonTemplate(using Universe) = ChangeWaterFloorButton().asTemplate()
+
+@definition def waterFlowingGround(using Universe) = WaterFlowingGround()
+@definition def waterFlowingHole(using Universe) = WaterFlowingHole()
+
+class FlowingWaterInfos(using ComponentInit) extends Component {
+  category = CatFloorFlowingWater
+
+  var waterFloor: Int = 2
+
+  def setWaterFloor(value: Int): Unit = {
+    if waterFloor == value then
+      return
+
+    // TODO sound
+    //for I := 0 to Master.PlayerCount-1 do
+    //  Master.Players[I].PlaySound('RushingWater.wav');
+
+    waterFloor = value
+
+    for component <- Mazes.posComponentsBottomUp do {
+      component match
+        case component: PlatformGround =>
+          component.position = component.position.map(_.withZ(value))
+        case component: PlatformWall =>
+          component.position = component.position.map(_.withZ(value - 1))
+        case _ =>
+          ()
+    }
+  }
+}
+
+class PlatformGround(using ComponentInit) extends PosComponent {
+  category = CatFloorFlowingWater
+  painter += "Fields/WoodFloor"
+
+  override def hookEntering(context: EnteringContext): Unit = ()
+  override def hookEntered(context: EnteredContext): Unit = ()
+}
+
+class PlatformWall(using ComponentInit) extends PosComponent {
+  category = CatFloorFlowingWater
+  painter += "Boxes/WoodenBox"
+
+  override def hookEntering(context: EnteringContext): Unit =
+    context.cancel()
+
+  override def hookEntered(context: EnteredContext): Unit = ()
+}
+
+class ChangeWaterFloorButton(using ComponentInit) extends PushButton {
+  category = CatFloorFlowingWater
+
+  var newWaterFloor: Int = 1
+
+  override def buttonDown(context: EnteredContext): Unit =
+    super.buttonDown(context)
+    flowingWaterInfos.setWaterFloor(newWaterFloor)
+}
+
+class WaterFlowingHole(using ComponentInit) extends Water {
+  category = CatFloorFlowingWater
+  painter += "Filters/NiceSoftDarken"
+
+  private val blackBox = Shape.Box(
+    Rectangle.sized(30, 30),
+    Fill.Color(RGBA.Black),
+    Stroke.None,
+    Point(15, 15),
+  )
+
+  override protected def doPresent(context: PresentSquareContext): Batch[SceneNode] =
+    if context.where.forall(_.z == flowingWaterInfos.waterFloor) then
+      super.doPresent(context)
+    else
+      blackBox +: DissipateNeighbors.presentDissipateGroundNeighbors(context)
+
+  override def entering(context: EnteringContext): Unit =
+    if flowingWaterInfos.waterFloor == context.pos.z then
+      super.entering(context)
+
+  override def entered(context: EnteredContext): Unit = {
+    import context.*
+
+    if flowingWaterInfos.waterFloor == pos.z then
+      // Act as water
+      super.entered(context)
+    else if flowingWaterInfos.waterFloor < pos.z then
+      // Drop down, like an EmptyField
+
+      if pos.z <= 0 then
+        return // just to be safe
+
+      player.sleep(200)
+
+      player.moveTo(pos.withZ(pos.z - 1), execute = true)
+  }
+
+  override def dispatch[A]: PartialFunction[SquareMessage[A], A] = {
+    case PlankInteraction(PlankInteraction.Kind.PassOver, player, pos, _, _) =>
+      flowingWaterInfos.waterFloor <= pos.z && !player.isAbleTo(GoOnWater)
+  }
+}
+
+class WaterFlowingGround(using ComponentInit) extends Ground {
+  category = CatFloorFlowingWater
+  painter += "Fields/Grass"
+
+  var lightenColor = RGBA(1, 1, 1, 95.0 / 255.0)
+
+  private def box = Shape.Box(
+    Rectangle.sized(30, 30),
+    Fill.Color(lightenColor),
+    Stroke.None,
+    Point(15, 15),
+  )
+
+  override protected def doPresent(context: PresentSquareContext): Batch[SceneNode] =
+    (super.doPresent(context) :+ box)
+      ++ DissipateNeighbors.presentDissipateNeighbors(context, _ == grass)
+}
---
+++ sources/WTMovableBlocks.scala
@@ -0,0 +1,65 @@
+package user.sjrd.templedeleau
+
+import com.funlabyrinthe.core.*
+import com.funlabyrinthe.mazes.*
+import com.funlabyrinthe.mazes.std.*
+
+import user.sjrd.movableblocks.*
+
+object WTMovableBlocks extends Module
+
+@definition def wtMovableBlockTemplate(using Universe) = WTMovableBlock().asTemplate()
+
+@definition def wtMovableBlockPuzzle1(using Universe) = WTMovableBlockPuzzle1()
+@definition def wtMovableBlockKeyPuzzle(using Universe) = WTMovableBlockKeyPuzzle()
+
+class WTMovableBlock(using ComponentInit) extends ConstrainedMovableBlock {
+  painter = painter.empty + "Blocks/SquareBlock"
+  maximumMoveCount = 1
+
+  override protected def isDestSquareValid(square: Square): Boolean = square match
+    case Square(_: Ground, e, t, o) => e.isEmpty && t.isEmpty && o.isEmpty
+    case _                          => false
+
+  override protected def isMoveAllowed(context: EnteringContext, target: SquareRef): Boolean = {
+    import context.*
+
+    super.isMoveAllowed(context, target) && {
+      if flowingWaterInfos.waterFloor > pos.z then
+        player.showMessageOnce(
+          "Sous l'eau, tu n'as pas de point d'appui. "
+            + "Tu ne peux donc pas pousser les blocs."
+        )
+        false
+      else
+        true
+    }
+  }
+
+  override protected def applyMove(context: EnteringContext, target: SquareRef): Unit = {
+    super.applyMove(context, target)
+    target().field match
+      case _: WaterFlowingHole => position = None
+      case _                   => ()
+  }
+}
+
+class WTMovableBlockPuzzle1(using ComponentInit) extends WTMovableBlock {
+  allowedDirs = Set(Direction.East)
+
+  override protected def applyMove(context: EnteringContext, target: SquareRef): Unit = {
+    super.applyMove(context, target)
+    fixThere()
+    context.map(19, 18, 3) = grass
+  }
+}
+
+class WTMovableBlockKeyPuzzle(using ComponentInit) extends WTMovableBlock {
+  allowedDirs = Set(Direction.North)
+
+  override protected def applyMove(context: EnteringContext, target: SquareRef): Unit = {
+    super.applyMove(context, target)
+    fixThere()
+    context.map(48, 13, 2) += silverKey
+  }
+}
